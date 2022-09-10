a933a7b1decd8b972685c2c7d177ccb002c28e69
Savenkova Mariya <sm3989@yandex.ru>
2022-09-11 01:30:45 +0300

    add program

---
+++ test_program/Program.cs
@@ -0,0 +1,29 @@
+﻿char[] delimiterChars = { '"', ',' };
+string s=Console.ReadLine();
+string[] array=s.Split(delimiterChars,StringSplitOptions.RemoveEmptyEntries);
+string[] newArray=new string[array.Length];
+correctLength(array);
+Print(newArray);
+
+
+void correctLength(string[] array)
+{
+    int count =0;
+    for (int i=0;i<array.Length;i++)
+    {
+        if(array[i].Length<=3)
+        {
+            newArray[count]=array[i];
+            count++;
+        }
+    }
+ }
+
+ void Print(string[] array)
+ {
+    array=array.Where(x=>x!=null).ToArray();
+    for (int i=0;i<array.Length;i++)
+     {
+        System.Console.Write($"{array[i]} ");
+     }
+ }
